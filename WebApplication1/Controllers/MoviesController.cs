4f370442114432e933d3829ff8246d34bbc67eaa
VeraV3 <veravukosavljevic0@gmail.com>
2023-05-06 23:25:44 +0200

    Add project files.

---
+++ WebApplication1/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+
+namespace WebApplication1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" }; //from database
+            return View(movie);//ekvivalentno reutn new ViewResult() ali kako ako    je ovo interferjs
+        }                      //prosledili smo MODEL pogledu 
+                               // postoje jos dva nacina viewDataDictionary i viewBag oba losa
+        public IActionResult PoDatumuPustanja(int godina, int mesec)
+        {
+            return Content(godina + "/" + mesec);
+        }
+        
+    }
+}
